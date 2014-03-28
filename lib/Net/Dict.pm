8c935d18d1b4ed5f34677d8d36ef6827441d95fd
Neil Bowers <neil@bowers.com>
2014-03-28 20:51:58 +0000

    Import of NEILB/Net-Dict-2.14 from CPAN.
    gitpan-cpan-distribution: Net-Dict
    gitpan-cpan-version:      2.14
    gitpan-cpan-path:         NEILB/Net-Dict-2.14.tar.gz
    gitpan-cpan-author:       NEILB
    gitpan-cpan-maturity:     released

--- lib/Net/Dict.pm
+++ lib/Net/Dict.pm
@@ -18,7 +18,7 @@ use Net::Cmd;
 use Carp;
 
 use vars qw(@ISA $debug);
-our $VERSION = '2.13';
+our $VERSION = '2.14';
 
 #-----------------------------------------------------------------------
 # Default values for arguments to new(). We also use this to
@@ -221,7 +221,7 @@ sub define
         my ($defNum) = ($self->message =~ /^\d{3} (\d+) /);
         foreach (0..$defNum-1)
         {
-            my ($d) = ($self->getline =~ /^\d{3} ".*" (\w+) /);
+            my ($d) = ($self->getline =~ /^\d{3} ".*" ([-\w]+) /);
             my ($def) = join '', @{$self->read_until_dot};
             push @defs, [$d, $def];
         }
